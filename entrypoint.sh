d4c2421288c905febe8e062165d0f8c2057b36a4
Rodolfo Neves <roddas360@gmail.com>
2025-11-09 17:47:31 -0300

    commit do desespero

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,18 @@ jupyter execute ./data/raw/analytics.ipynb
 
 echo "Raw Data Analysis Notebook executed successfully!"
 
-echo "Starting ETL process from Raw to Silver..."
+echo "Starting ETL process"
+echo "\tETL Raw -> Silver..."
 
 jupyter execute ./etl/etl_raw_to_silver.ipynb
+
+echo "ETL process from Raw to Silver..."
+
+jupyter execute ./etl/etl_slv_gold.ipynb
+echo "\tETL Silver -> Gold..."
 
 echo "ETL process completed successfully!"
 
 echo "Starting Jupyter Lab..."
 
-jupyter lab --NotebookApp.token=''
+jupyter lab --NotebookApp.token='' --allow-root
