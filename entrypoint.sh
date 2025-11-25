a1e8a2b72fa621705d3764a45472ed4eb4f57618
Arthur Ribeiro <artrsousa1@gmail.com>
2025-11-25 20:11:36 -0300

    feat: add silver to gold elt to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,10 @@ echo "Starting ETL process from Raw to Silver..."
 
 jupyter execute ./transformer/etl/etl_raw_to_silver.ipynb
 
+echo "Starting ETL process from Silver to Gold..."
+
+jupyter execute ./transformer/etl/etl_silver_to_gold.ipynb
+
 echo "ETL process completed successfully!"
 
 echo "Starting Jupyter Lab..."
